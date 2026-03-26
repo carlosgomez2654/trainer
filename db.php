122c0c5d7194843e7c9f87949014bcd99e99631e
Carlos Gomez <carlosgomez2654@gmail.com>
2026-03-26 21:29:04 +1000

    Actualizando datos de conexion Clever Cloud para Entrenador

--- db.php
+++ db.php
@@ -1,12 +1,17 @@
 <?php
-$host = "sql203.infinityfree.com"; // Revisa en tu panel si es sql203 o sql301
-$user = "if0_41479032";            // Tu nombre de usuario de vPanel
-$pass = "TuPasswordDeInfinity";    // El password de tu cuenta de hosting (no el de GitHub)
-$db   = "if0_41479032_entrenador_personal";
+$host = "bg7b1azbx9c4bjnmxvni-mysql.services.clever-cloud.com";
+$user = "u9iiujwt53caa5zg";
+$pass = "YlzWGw7nsQRZH82UKg7p";
+$db   = "bg7b1azbx9c4bjnmxvni";
+$port = 3306;
 
-$conexion = mysqli_connect($host, $user, $pass, $db);
+// Conexión incluyendo el puerto para mayor seguridad
+$conexion = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
+
+// Esto es para que las tildes y la 'ñ' se vean bien en tu página
+mysqli_set_charset($conexion, "utf8");
 ?>
